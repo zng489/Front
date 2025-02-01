815647f4cbcf5b216ce131bbb9b4dd728db6eb5c
unknown <zhang489@hotmail.com>
2025-02-01 08:46:53 -0300

    arquivos_de_front

---
+++ angular dashboard/src/app/top-widgets/top-widgets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-widgets',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-widgets.component.html',
+  styleUrl: './top-widgets.component.scss'
+})
+export class TopWidgetsComponent {
+
+}
